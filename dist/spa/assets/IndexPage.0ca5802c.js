8588d695ab238f4140e7e3a39f46c13d9ecf64f8
Nikocards <niikosaul@gmail.com>
2024-04-16 00:35:01 +0200

    Version final

---
+++ dist/spa/assets/IndexPage.0ca5802c.js
@@ -0,0 +1,1 @@
+import{Q as e}from"./QPage.1a996c5e.js";import{I as a,K as t,L as n}from"./index.e2e8a6b5.js";import"./render.78f7790d.js";const _=a({name:"IndexPage",__name:"IndexPage",setup(o){return(r,s)=>(t(),n(e,{class:"row items-center justify-evenly"}))}});export{_ as default};
